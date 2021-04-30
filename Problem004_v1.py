a290d0098a46b81bcce5cfe04d7ea9de8fd478d3
RNTay <81459529+RNTay@users.noreply.github.com>
2021-04-30 22:22:51 +0800

    reduce number of loops needed
    set a ≥ b

--- Problem004_v1.py
+++ Problem004_v1.py
@@ -3,10 +3,10 @@
 largest = 0
 for a in range(100, 1000):
     for b in range(100, 1000):
-        n = a*b
-        s = str(n)
-        if s == s[::-1]:
-            if n > largest:
-                largest = n
-
+        if a >= b:
+            n = a*b
+            s = str(n)
+            if s == s[::-1]:
+                if n > largest:
+                    largest = n
 print(largest)
