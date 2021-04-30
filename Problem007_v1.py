d4134e3fd7366fc9077bd6ce1267dec4080f3914
RNTay <81459529+RNTay@users.noreply.github.com>
2021-04-30 22:37:30 +0800

    Create Problem007_v1.py

---
+++ Problem007_v1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+# Primality test code taken from Wikipedia
+def is_prime(n: int) -> bool:
+    """Primality test using 6k+-1 optimization."""
+    if n <= 3:
+        return n > 1
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i ** 2 <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+
+nums = 3  # skip 2 to allow for +=2 instead of += 1
+index = 1
+last_prime = 0
+while index < 10001:
+    if is_prime(nums):
+        last_prime = nums
+        index += 1
+    nums += 2
+print(last_prime)
