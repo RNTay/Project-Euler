505cefed7b12bb95a44c42c63052bff32d3007ae
RNTay <81459529+RNTay@users.noreply.github.com>
2021-05-14 01:48:01 +0800

    Create Problem010_v2.py
    this is significantly faster than the solution in Problem010_v1.py

---
+++ Problem010_v2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+def primes_below(n: int) -> list:
+    """Returns the list all primes [2 .. n] using
+    the sieve of Eratosthenes."""
+    if n <= 1:
+        return []
+
+    index_is_prime = [False, False]
+    index_is_prime += [True for _ in range(2, n+1)]
+
+    sqrt_n = n**0.5
+    for i in range(2, int(sqrt_n)+1):
+        if index_is_prime[i]:
+            for j in range(i**2, n+1, i):
+                index_is_prime[j] = False
+
+    primes_list = []
+    for p in range(n+1):
+        if index_is_prime[p]:
+            primes_list.append(p)
+
+    return primes_list
+
+
+limit = 1_999_999
+print(sum(primes_below(limit)))
