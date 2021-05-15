0b105a6b74dd642110bee27f69b9cc927c8b175c
RNTay <81459529+RNTay@users.noreply.github.com>
2021-05-15 17:04:41 +0800

    created v2, store digits in file

---
+++ Problem008_v2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# save numbers in text_file.txt, ending the file with a newline.
+
+digits_file = open('text_file.txt', 'r')
+digits_line = digits_file.readline()
+digits_string = ''
+while len(digits_line) > 0:
+    digits_string += digits_line[:-1]
+    digits_line = digits_file.readline()
+digits_file.close()
+
+greatest_product = 0
+for a in range(0, 1000-13):
+    thirteen_string = digits_string[a:a+13]
+    product = 1
+    for i in thirteen_string:
+        product = product * int(i)
+    if product > greatest_product:
+        greatest_product = product
+print(greatest_product)
