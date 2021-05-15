e99a6c7cf19a2ce43edef742a60d27af187b5199
RNTay <81459529+RNTay@users.noreply.github.com>
2021-05-16 02:01:46 +0800

    Create Problem019.py

---
+++ Problem019.py
@@ -0,0 +1,10 @@
+import datetime
+
+this_day = datetime.date(1901, 1, 1)
+count_sundays = 0
+while this_day != datetime.date(2001, 1, 1):
+    if this_day.day == 1:
+        if this_day.isoweekday() == 7:
+            count_sundays += 1
+    this_day += datetime.timedelta(days=1)
+print(count_sundays)
