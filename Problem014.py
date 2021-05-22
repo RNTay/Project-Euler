ed7a6f27c9c6f6e4e801208fc53d945e24af5743
RNTay <81459529+RNTay@users.noreply.github.com>
2021-05-22 18:22:48 +0800

    Create Problem014.py

---
+++ Problem014.py
@@ -0,0 +1,22 @@
+from functools import cache
+
+
+@cache
+def collatz_length(n):
+    if n == 1:
+        return 1
+    else:
+        if n % 2 == 0:
+            return 1 + collatz_length(n//2)
+        else:
+            return 2 + collatz_length((3*n+1) // 2)
+
+
+longest = 0
+number_with_longest_chain = 0
+for x in range(1, 1_000_001):
+    this_length = collatz_length(x)
+    if this_length > longest:
+        longest = this_length
+        number_with_longest_chain = x
+print(number_with_longest_chain)
