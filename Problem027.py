d92218387ec242c0f8adec2377f3f300b373e09d
RNTay <81459529+RNTay@users.noreply.github.com>
2021-05-26 20:54:47 +0800

    Create Problem027.py

---
+++ Problem027.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+def is_prime(p: int) -> bool:
+    """Primality test using 6k+-1 optimisation."""
+    if p <= 3:
+        return p > 1
+    if p % 2 == 0 or p % 3 == 0:
+        return False
+    i = 5
+    while i ** 2 <= p:
+        if p % i == 0 or p % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+
+max_number = 0
+max_a, max_b = 0, 0
+for a in range(-999, 1000):
+    for b in range(-999, 1000):
+        count_primes = 0
+        n = 0
+        while is_prime(n**2 + a*n + b):
+            n += 1
+        if n > max_number:
+            max_number = n
+            max_a, max_b = a, b
+print(max_a * max_b)
