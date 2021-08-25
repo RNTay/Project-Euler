ef9ee4cbf7785b26bcb9fdd7ed1e8b94fcbfc648
RNTay <81459529+RNTay@users.noreply.github.com>
2021-08-26 01:40:16 +0800

    guaranteed minimum D solution

--- Problem044.py
+++ Problem044.py
@@ -1,29 +1,55 @@
 #!/usr/bin/env python3
 
-def pentagon_sum_and_difference(a, b):
-    w_sum = 3 * (a ** 2 + b ** 2) - a - b
-    sqrt_w_sum = (12 * w_sum + 1) ** 0.5
-    if int(sqrt_w_sum) == sqrt_w_sum:
-        if (sqrt_w_sum + 1) % 6 == 0:
-            w_diff = 3 * (a ** 2 - b ** 2) - a + b
-            if w_diff > 0:
-                sqrt_w_diff = (12 * w_diff + 1) ** 0.5
-                if int(sqrt_w_diff) == sqrt_w_diff:
-                    if (sqrt_w_diff + 1) % 6 == 0:
-                        # return (sqrt_w_sum + 1) // 6, (sqrt_w_diff + 1) // 6
-                        return True
-    # return -1, -1
+def sum_is_pentagon(j, k):
+    w = 3 * (j ** 2 + k ** 2) - j - k
+    sqrt = (12 * w + 1) ** 0.5
+    if sqrt == int(sqrt):
+        if (int(sqrt) + 1) % 6 == 0:
+            return True
     return False
 
 
-loop = True
-x = 1
-while loop:
-    for y in range(1, x + 1):
-        if pentagon_sum_and_difference(x, y):
-            print(x*(3*x - 1)//2 - y*(3*y - 1)//2)
-            loop = False
-            break
-    x += 1
+n = 1
+D = n*(3*n - 1) // 2
+
+go_next = True
+while go_next:
+    tried_A = set()
+    for A in range(1, int(D**0.5) + 1):
+        for i in range(1, 3):  # i = 1, 2
+            a = A / i
+            b = D / a
+            if b == int(b):
+                tried_A.add(A)
+                x = (2*a + 3*b + 1) / 6
+                y = x - b
+
+                if (x == int(x)) and (y > 0):
+                    x = int(x)
+                    y = int(y)
+
+                    if sum_is_pentagon(x, y):
+                        print(D, x, y)
+                        go_next = False
+                        break
+    for b in tried_A:
+        for i in range(1, 3):  # i = 1, 2
+            a = (D / b) / i
+            b *= i
+
+            x = (2*a + 3*b + 1) / 6
+            y = x - b
+
+            if (x == int(x)) and (y > 0):
+                x = int(x)
+                y = int(y)
+
+                if sum_is_pentagon(x, y):
+                    print(D)
+                    go_next = False
+                    break
+
+    n += 1
+    D = n*(3*n - 1) // 2
 
 
