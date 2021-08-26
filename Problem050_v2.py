b9d56f90b427053d28aca41708e81e213e26c246
RNTay <81459529+RNTay@users.noreply.github.com>
2021-08-26 20:29:12 +0800

    removed unnecessary print statement

--- Problem050_v2.py
+++ Problem050_v2.py
@@ -42,7 +42,6 @@ primes = primes_up_to(int(limit/21) + 21)
 best_prime, best_length = 0, 0
 
 for a in range(len(primes) - 1):
-    print(primes[a])
     consecutive_sum = primes[a]
     sum_length = 1
     this_best_prime, this_best_length = 0, 0
