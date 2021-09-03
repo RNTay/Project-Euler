c82950759191e2380527243098e50a216fb9fcaa
RNTay <81459529+RNTay@users.noreply.github.com>
2021-09-04 00:10:20 +0800

    Create Problem099.py

---
+++ Problem099.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from math import log2
+
+largest = 0
+line_counter = 0
+best_line = 0
+with open('text-file.txt', 'r') as base_exps_file:
+    for line in base_exps_file:
+        line_counter += 1
+        base, exp = tuple(map(int, line[:-1].split(',')))
+        this_base_exp = exp * log2(base)
+        if this_base_exp > largest:
+            largest = this_base_exp
+            best_line = line_counter
+print(best_line)
+
+
