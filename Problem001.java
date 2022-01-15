53537dee3b5e74c2f884ed1071d88d2a030b867e
RNTay <81459529+RNTay@users.noreply.github.com>
2022-01-15 13:00:38 +0000

    Create Problem001.java

---
+++ Problem001.java
@@ -0,0 +1,12 @@
+class sandbox {
+    public static void main(String[] args) {
+        int number = 1, sum = 0;
+        while (number < 1000) {
+            if ((number % 3 == 0) || (number % 5 == 0)) {
+                sum += number;
+            }
+            number ++;
+        }
+        System.out.println(sum);
+    }
+}
